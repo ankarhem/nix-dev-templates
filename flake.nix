ec59e0e5510eb533090cbeb0699e12fa2257b644
Jakob Ankarhem <jakob@ankarhem.dev>
2026-01-09 12:22:53 +0100

    base flake

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    git-hooks.url = "github:cachix/git-hooks.nix";
+    git-hooks.inputs.nixpkgs.follows = "nixpkgs";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+
+  outputs =
+    inputs@{ self, ... }:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.git-hooks.flakeModule
+        inputs.treefmt-nix.flakeModule
+      ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          lib,
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          treefmt = {
+            programs.nixfmt.enable = true;
+            programs.nixfmt.package = pkgs.nixfmt;
+          };
+          pre-commit.settings.hooks = {
+            treefmt.enable = true;
+          };
+          devShells.default = pkgs.mkShell {
+            inherit (config.pre-commit) shellHook;
+            packages = config.pre-commit.settings.enabledPackages;
+          };
+        };
+    };
+}
